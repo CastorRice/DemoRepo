55700d11191ad0364b00877bcd73d3236b811e12
CastorRice <castor.rice@outlook.com>
2024-10-15 19:51:37 +0200

    Added example of equals on memory

--- main.java
+++ main.java
@@ -1,6 +1,27 @@
 public class main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    Student a = new Student("Peter Parker", 21);
+    Student b = new Student("Walter White", 55);
+    Student c = a;
+    System.out.println(a.equals(b));
+    System.out.println(a.equals(c));
+  }
+
+  static class Student{
+    private String name;
+    private int age;
+
+    public Student(String name, int age){
+      this.name = name;
+      this.age = age;
+    }
+    public String getName() {
+      return name;
+    }
+    public int getAge() {
+      return age;
+    }
   }
 }
